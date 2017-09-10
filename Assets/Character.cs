a956a20ab6ece86163d8f5ca3eced19cb7c137d0
Justin Park <justindypark@yahoo.com>
2017-09-10 12:57:39 -0700

    Uploading initial files (I forgot to put this to GitHub initially)

---
+++ Assets/Character.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	bool walk = false;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(walk) {
+			anim.SetBool ("Walk", true);
+		} else {
+			anim.SetBool ("Walk", false);
+		}
+		if(Input.GetKey("space")) {
+			walk = true;
+		} else {
+			walk = false;
+		}
+	}
+}
